e5d48b6eb7b25e6a0b707d093990b003da72d011
zykcode <zyskson@gmail.com>
2024-07-29 06:48:30 +0100

    last unfinshed

--- src/components/post-scroll.tsx
+++ src/components/post-scroll.tsx
@@ -1,18 +1,17 @@
-'use client';
-import React, { useRef } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
-import Image from 'next/image';
+import React from 'react';
 import PostCard from './post-card';
 
-const BlogPostScroll = ({ posts, image }) => {
-  <div className="flex flex-col md:flex-row">
-    <div className="h1">Posts</div>
-    <div className="flex flex-col">
-      {posts.map((post, index) => (
-        <PostCard post={post} key={index} />
-      ))}
+const BlogPostScroll = ({ posts }) => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="h1">Posts</div>
+      <div className="flex flex-col">
+        {posts.map((post, index) => (
+          <PostCard post={post} key={index} />
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default BlogPostScroll;
