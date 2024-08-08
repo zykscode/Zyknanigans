90d0c45e73d27d8eaffcb38c001c59e44fb99464
zykcode <zyskson@gmail.com>
2024-08-08 18:59:47 +0100

    feat: page

--- src/components/post-scroll.tsx
+++ src/components/post-scroll.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PostCard from './post-card';
 
 const BlogPostScroll = ({ posts }) => {
+  // Create a new array where each post is repeated 5 times
+  const extendedPosts = posts.flatMap(post => Array(5).fill(post));
+
   return (
-     <div className="container max-w-4xl py-6 lg:py-10">
-    
-      <div className="flex flex-col">
-        {posts.map((post, index) => (
-          <PostCard post={post} image={post.coverImage} key={post.title} />
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="grid gap-10 sm:grid-cols-2">
+        {extendedPosts.map((post, index) => (
+          <PostCard post={post} image={post.coverImage} key={index} />
         ))}
       </div>
     </div>
